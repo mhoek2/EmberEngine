a628fc21477400e34bf3d210554ea62f34a14e03
Marco H <153363103+mhoek2@users.noreply.github.com>
2024-09-30 13:52:27 +0200

    Add spherre, cube and mesh gameObject type

---
+++ gameObjects/mesh.py
@@ -0,0 +1,64 @@
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+import numpy as np
+
+from modules.files import FileHandler
+from gameObjects.gameObject import GameObject
+
+import pathlib
+from objloader import *
+
+class Mesh( GameObject ):
+    def __init__( self, translate=( 0.0, 0.0, 0.0 ), rotation=( 0.0, 0.0, 0.0, 0.0 ), filename="" ) -> None:
+        self.translate = translate
+        self.rotation = rotation
+        self.filename = filename;
+
+        self.vertices = []
+        self.loadMesh()
+        
+        #self.file = FileHandler( self.filename )
+        #self.model = Obj.fromstring( self.file.getContent() )
+        return
+    
+    def loadMesh( self ) -> None:
+        self.file = FileHandler( self.filename )
+        obj_file = pathlib.Path( self.filename )
+        
+        with obj_file.open('r') as file:
+            for line in file:
+                if line.startswith( 'v ' ):  # Vertex line
+                    parts = line.split()  # Split the line into parts
+                    self.vertices.append( ( float(parts[1]), float(parts[2]), float(parts[3]) ) )
+    
+
+        # transition to glTranslatef please
+        #self.translated_vertices = [
+        #    (vertex[0] + self.position[0], 
+        #     vertex[1] + self.position[1], 
+        #     vertex[2] + self.position[2])
+        #    for vertex in self.vertices
+        #]
+
+        return
+
+    def update( self ) -> None:
+        return
+
+    def draw( self ) -> None:
+        glPushMatrix()
+        glRotatef( self.rotation[0], self.rotation[1], self.rotation[2], self.rotation[3] ); 
+        glTranslatef( self.translate[0], self.translate[1], self.translate[2] ); 
+
+        glBegin( GL_TRIANGLES )
+        for vertex in self.vertices:
+            glColor3f( 1.0, 1.0, 1.0 )
+            glVertex3f( vertex[0], vertex[1], vertex[2] )
+        glEnd()
+
+        glPopMatrix()
+        return
--- main.py
+++ main.py
@@ -7,64 +7,21 @@ from OpenGL.GLU import *
 import math
 
 from modules.renderer import Renderer
+from gameObjects.cube import Cube
+from gameObjects.sphere import Sphere
+from gameObjects.mesh import Mesh
 
 renderer = Renderer()
 renderer.setup_frustum_mvp()
 sphere = gluNewQuadric() 
 renderer.setup_lighting()
 
-
-def draw_cube():
-    """Draws a wireframe cube with colored axes."""
-    # Define the vertices of the cube
-    vertices = [
-        (1, 1, -1),  # Top right front
-        (1, -1, -1), # Bottom right front
-        (-1, -1, -1),# Bottom left front
-        (-1, 1, -1), # Top left front
-        (1, 1, 1),   # Top right back
-        (1, -1, 1),  # Bottom right back
-        (-1, -1, 1), # Bottom left back
-        (-1, 1, 1),  # Top left back
-    ]
-
-    # Define the edges connecting the vertices
-    edges = [
-        (0, 1), (1, 2), (2, 3), (3, 0),  # Front face
-        (4, 5), (5, 6), (6, 7), (7, 4),  # Back face
-        (0, 4), (1, 5), (2, 6), (3, 7)   # Connect front and back
-    ]
-
-    # Draw the edges of the cube
-    glBegin(GL_LINES)
-    for edge in edges:
-        for vertex in edge:
-            glColor3f(1.0, 1.0, 1.0)  # White color for cube edges
-            glVertex3fv(vertices[vertex])
-    glEnd()
-
-    # Draw colored axes
-    # X-axis (Red)
-    glBegin(GL_LINES)
-    glColor3f(1.0, 0.0, 0.0)  # Red color
-    glVertex3f(-2, 0, 0)  # Start point of X-axis
-    glVertex3f(2, 0, 0)   # End point of X-axis
-    glEnd()
-
-    # Y-axis (Green)
-    glBegin(GL_LINES)
-    glColor3f(0.0, 1.0, 0.0)  # Green color
-    glVertex3f(0, -2, 0)  # Start point of Y-axis
-    glVertex3f(0, 2, 0)   # End point of Y-axis
-    glEnd()
-
-    # Z-axis (Blue)
-    glBegin(GL_LINES)
-    glColor3f(0.0, 0.0, 1.0)  # Blue color
-    glVertex3f(0, 0, -2)  # Start point of Z-axis
-    glVertex3f(0, 0, 2)   # End point of Z-axis
-    glEnd()
-
+gameObjects = []
+gameObjects.append( Cube( translate=(0, 0, 0), rotation=(45, 1, 1, 1) ) )
+gameObjects.append( Sphere( translate=(-4, 0, 0) ) )
+gameObjects.append( Sphere( translate=(0, 0, 0) ) )
+gameObjects.append( Sphere( translate=(-2, 0, 0) ) )
+gameObjects.append( Mesh( translate=(-16, 0, 0), filename="C:/Github-workspace/EmberEngine/assets/models/Tree/tree.obj" ) )
 
 while renderer.running:
     events = pygame.event.get()
@@ -82,17 +39,11 @@ while renderer.running:
         glVertex3f( -10, 10, -2 )
         glEnd()
 
-        draw_cube()
+        for gameObject in gameObjects:
+            gameObject.update();
 
-        # sphere
-        glTranslatef (3, 0, 2 )
-        glColor4f( 0.9, 0.9, 0.9, 1 )
-        gluSphere( sphere, 1.0, 32, 16 ) 
-
-        # sphere
-        glTranslatef( -3, 0, -2 )
-        glColor4f( 0.9, 0.9, 0.0, 1 )
-        gluSphere( sphere, 1.0, 32, 16 ) 
+        for gameObject in gameObjects:
+            gameObject.draw();
 
         renderer.end_frame()
 pygame.quit()
