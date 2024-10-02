bd2be86de22d6bd18a9fabf0c3b9476f306685e7
Marco H <153363103+mhoek2@users.noreply.github.com>
2024-10-02 11:29:57 +0200

    Create class for EmberEngine to use context in sub modules

--- gameObjects/gameObject.py
+++ gameObjects/gameObject.py
@@ -7,9 +7,10 @@ from OpenGL.GLU import *
 import numpy as np
 
 class GameObject:
-    def __init__( self, renderer, textures, translate=( 0.0, 0.0, 0.0 ), rotation=( 0.0, 0.0, 0.0 ), scale=( 1.0, 1.0, 1.0 ) ) -> None:
-        self.renderer = renderer
-        self.textures = textures
+    def __init__( self, context, translate=( 0.0, 0.0, 0.0 ), rotation=( 0.0, 0.0, 0.0 ), scale=( 1.0, 1.0, 1.0 ) ) -> None:
+        self.context = context
+        self.renderer = context.renderer
+        self.textures = context.textures
         
         # https://github.com/adamlwgriffiths/Pyrr
         self.translate = translate
--- main.py
+++ main.py
@@ -19,89 +19,50 @@ from gameObjects.fullcube import FullCube
 
 from modules.TextureLoader import load_texture_pygame as load_texture
 
-renderer = Renderer()
-renderer.setup_projection()
+class EmberEngine:
+    def __init__( self ) -> None:  
+        self.renderer = Renderer()
+        self.renderer.setup_projection()
 
-gameObjects = []
+        self.gameObjects = []
 
-textures = glGenTextures(3)
-load_texture("C:/Github-workspace/EmberEngine/assets/textures/cube.png", textures[0])
+        self.textures = glGenTextures(3)
+        load_texture( "C:/Github-workspace/EmberEngine/assets/textures/cube.png", self.textures[0] )
 
-def addGameObject( object ) -> int:
-    index = len(gameObjects)
-    gameObjects.append( object )
-    return index
+        self.addGameObject( FullCube( self,
+                                    translate=(-2, 0, 0),
+                        ) )
 
-sun = addGameObject( Sun( renderer, 
-                          translate=(-2, 0, 0), 
-                          color=(0.7, 0.7, 0.0),
-                          diffuse=(.0, 1.0, 0.7),
-                          ambient=(0.0, 0.2, 0.2),
-                          anim_radius=10,
-                          anim_speed=2
-                   ) )
+        self.addGameObject( FullCube( self,
+                                    translate=(2, 0, 0),
+                                    scale=( 0.5, 0.5, 0.5 ),
+                                    rotation=( 0.5, 0.0, 0.0 )
+                        ) )
 
-addGameObject( FullCube( renderer, textures,
-                         translate=(-2, 0, 0),
-             ) )
+        # bind projection matrix
+        glUniformMatrix4fv( self.renderer.uPMatrix, 1, GL_FALSE, self.renderer.projection )
 
-addGameObject( FullCube( renderer, textures,
-                         translate=(2, 0, 0),
-                         scale=( 0.5, 0.5, 0.5 ),
-                         rotation=( 0.5, 0.0, 0.0 )
-             ) )
+    def addGameObject( self, object ) -> int:
+        index = len( self.gameObjects )
+        self.gameObjects.append( object )
 
-file = JsonHandler( 'C:/Github-workspace/EmberEngine/json/lang.json' )
-json_content = file.getJson();
+        return index
 
-def JsonToVertices() -> None:
-    gameObjects.append( Cube() )
+    def run( self ) -> None:
+        while self.renderer.running:
+            events = pygame.event.get()
+            self.renderer.event_handler( events )
 
-    scale_modifier = 1
+            if not self.renderer.paused:
+                self.renderer.begin_frame()
 
-    for lang in json_content['languages']:
+                # trigger update function in registered gameObjects
+                for gameObject in self.gameObjects:
+                    gameObject.onUpdate();
 
-        coords = lang['coordinates']
+                self.renderer.end_frame()
+        pygame.quit()
 
-        translation = ( coords['x'] * scale_modifier,
-                        coords['y'] * scale_modifier,
-                        coords['z'] * scale_modifier
-        )
-
-        color = (1.0, 1.0, 1.0) 
-        radius = 0.02
-
-        if lang['name'] == "Python":
-            color = (1.0, 1.0, 0.0) 
-            radius = 0.05
-
-        elif lang['name'] == "C++":
-            color = (0.0, 1.0, 1.0)
-            radius = 0.05
-
-
-        gameObjects.append( Sphere( 
-            translate=translation,
-            radius=radius,
-            stacks=5,
-            slices=10,
-            color=color
-        ) )
-
-JsonToVertices()
-
-glUniformMatrix4fv( renderer.uPMatrix, 1, GL_FALSE, renderer.projection )
-
-while renderer.running:
-    events = pygame.event.get()
-    renderer.event_handler( events )
-
-    if not renderer.paused:
-        renderer.begin_frame()
-
-        # trigger update function in registered gameObjects
-        for gameObject in gameObjects:
-            gameObject.onUpdate();
-
-        renderer.end_frame()
-pygame.quit()
+if __name__ == '__main__':
+    app = EmberEngine()
+    app.run()
