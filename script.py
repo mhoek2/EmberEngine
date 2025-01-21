6e20a18fe7e64ccb5e10dac9d9c5b3f01f695bb5
mhoek2 <153363103+mhoek2@users.noreply.github.com>
2025-01-21 13:50:39 +0100

    Add basic gameobject and camera controller

--- script.py
+++ script.py
@@ -1,8 +1,30 @@
+import pygame
+
 class MyScript:
     """Default script template"""
     def onStart( self ) -> None:
-        print( f"gameobjects scale: {self.gameObject.scale}")
+        self.camera = self.context.gameObjects[self.context.camera_object]
+
+        pass
 
     def onUpdate( self ) -> None:
-        print( f"gameobjects scale: {self.gameObject.scale}")
-        pass
+        keypress = self.key.get_pressed()
+        velocity = 0.5
+        
+        if keypress[pygame.K_LCTRL] or keypress[pygame.K_RCTRL]:
+            velocity *= 10
+
+        move = self.renderer.deltaTime * velocity
+
+        if keypress[pygame.K_w]:
+            self.translate[2] += move
+            self.camera.translate[2] += move
+        if keypress[pygame.K_s]:
+            self.translate[2] -= move
+            self.camera.translate[2] -= move
+        if keypress[pygame.K_d]:
+            self.translate[0] -= move
+            self.camera.translate[0] -= move
+        if keypress[pygame.K_a]:
+            self.translate[0] += move
+            self.camera.translate[0] += move
